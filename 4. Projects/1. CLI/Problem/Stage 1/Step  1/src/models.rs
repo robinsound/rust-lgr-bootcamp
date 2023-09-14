16d999b7f81bbad8a0b306a3e1e059fb06768236
robinsound <robinsound@gmail.com>
2023-09-14 07:17:01 +0100

    Step 1 done

--- 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
+++ 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
@@ -1,28 +1,56 @@
+use std::collections::HashMap;
+
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
+    // add fields (make sure the fields are public)
+    // status can be Open, InProgress, Resolved or Closed.
+    Open,
+    InProgress,
+    Resolved,
+    Closed
 }
 
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    // add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<i32>
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        // by default the status should be set to open and the stories should be an empty vector
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new()
+        }
     }
 }
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    // add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        // by default the status should be set to open
+        Self {
+            name,
+            description,
+            status: Status::Open
+        }
     }
 }
 
 pub struct DBState {
     // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
+    // add fields (make sure the fields are public)
+    pub last_item_id: i32,
+    pub epics: HashMap<i32, Epic>,
+    pub stories: HashMap<i32, Story>
 }
